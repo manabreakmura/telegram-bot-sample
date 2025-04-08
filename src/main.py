1303b79fff39840723ec5bbc04306fe0829b39b0
manabreakmura <174273321+manabreakmura@users.noreply.github.com>
2025-04-09 00:36:01 +0300

    Add aiosqlite

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime, timedelta
 from itertools import batched
 
+import aiosqlite
 from decouple import config
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
@@ -13,6 +14,8 @@ from telegram.ext import (
     ConversationHandler,
 )
 
+DATABASE_URL = config("DATABASE_URL")
+
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
@@ -20,11 +23,47 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 date_state, time_state, location_state = range(3)
-answers = {}
+results = {}
 
 
 async def start_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    now = datetime.now().date()
+    async with aiosqlite.connect(DATABASE_URL) as db:
+        await db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users 
+            (id INTEGER PRIMARY KEY,
+            telegram_id INTEGER NOT NULL UNIQUE,
+            first_name TINYTEXT NOT NULL,
+            username TINYTEXT NULL)
+            """
+        )
+
+        await db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS orders
+            (id INTEGER PRIMARY KEY,
+            date TINYTEXT NOT NULL,
+            time TINYTEXT NOT NULL,
+            location TINYTEXT NULL,
+            telegram_id INTEGER NULL,
+            FOREIGN KEY (telegram_id) REFERENCES users(telegram_id) ON DELETE CASCADE)
+            """
+        )
+
+        await db.execute(
+            """
+            INSERT OR IGNORE INTO users (telegram_id, first_name, username) VALUES (?, ?, ?)
+            """,
+            (
+                update.effective_user.id,
+                update.effective_user.first_name,
+                update.effective_user.username,
+            ),
+        )
+
+        await db.commit()
+
+    now = datetime.now()
     buttons = []
     for x in range(8):
         date = (now + timedelta(days=x)).strftime("%d %B %Y")
@@ -44,7 +83,7 @@ async def start_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 async def date_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     query = update.callback_query
     await query.answer()
-    answers["date"] = query.data
+    results["date"] = query.data
 
     keyboard = [
         [
@@ -65,7 +104,7 @@ async def date_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
 async def time_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     query = update.callback_query
     await query.answer()
-    answers["time"] = query.data
+    results["time"] = query.data
 
     keyboard = [
         [
@@ -82,11 +121,28 @@ async def time_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
 async def location_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     query = update.callback_query
     await query.answer()
+    results["location"] = query.data
+    await query.delete_message()
 
-    answers["location"] = query.data
-    await query.edit_message_text(
-        text=f"{answers['date']}\n{answers['time']}\n{answers['location']}"
+    async with aiosqlite.connect(DATABASE_URL) as db:
+        await db.execute(
+            """
+            INSERT INTO orders (date, time, location, telegram_id) VALUES (?, ?, ?, ?)
+            """,
+            (
+                results["date"],
+                results["time"],
+                results["location"],
+                update.effective_user.id,
+            ),
+        )
+        await db.commit()
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=", ".join([value for value in results.values()]),
     )
+
     return ConversationHandler.END
 
 
@@ -97,10 +153,33 @@ async def cancel_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     return ConversationHandler.END
 
 
-async def error_handler(update: Update, context: CallbackContext):
+async def error_handler(update: Update, context: CallbackContext) -> None:
     logging.error(f'Update "{update}" caused error "{context.error}"')
-    error_message = "Something went wrong. We're looking into it."
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=error_message)
+    if update:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Something went wrong. We're looking into it.",
+        )
+
+
+async def total_command(update: Update, context: CallbackContext):
+    results = []
+    async with aiosqlite.connect(DATABASE_URL) as db:
+        async with db.execute(
+            """
+            SELECT users.username, users.telegram_id, users.first_name, orders.date, COUNT (*)
+            FROM users LEFT JOIN (orders) ON users.telegram_id = orders.telegram_id
+            GROUP BY orders.date
+            HAVING COUNT (*) >= 1
+            ORDER BY orders.date DESC
+            """
+        ) as rows:
+            async for row in rows:
+                results.append(str(row))
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="\n".join(results)
+    )
 
 
 if __name__ == "__main__":
@@ -116,5 +195,6 @@ if __name__ == "__main__":
         allow_reentry=True,
     )
     application.add_handler(conversation_handler)
+    application.add_handler(CommandHandler("total", total_command))
     application.add_error_handler(error_handler)
     application.run_polling()
